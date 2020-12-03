f4241d2e291ee5d4d2382c5bd3c2bf94f17079a4
Wasym <wasymshykh@gmail.com>
2020-12-03 15:10:43 +0500

    all value done

--- calculations.js
+++ calculations.js
@@ -1,5 +1,15 @@
+/* Helpers */
+
+const _c = (t) => {
+    return Number(t ? t : 0);
+}
+
 const _s = (n) => {
     return document.querySelector(n);
+}
+
+function _ro(n) {    
+    return +(Math.round(n + "e+2")  + "e-2");
 }
 
 /* Selecting Inputs */
@@ -25,11 +35,21 @@ const t_gesamt_r = _s("#gesamt-r");
 const t_einheit_r = _s("#einheit-r");
 const t_eust_einheit_r = _s("#eust-einheit-r");
 
-/* Helpers */
+/* Selecting Inputs - for amazon */
 
-const _c = (t) => {
-    return Number(t ? t : 0);
-}
+const i_verk = _s("#verk");
+const i_verkauf = _s("#verkauf");
+const i_versand = _s("#versand");
+
+/* Selecting Text - for amazon */
+
+const t_enthaltene = _s("#enthaltene");
+const t_gebuhren = _s("#gebuhren");
+const t_kosten = _s("#kosten");
+const t_spreis = _s("#spreis");
+const t_differenz = _s("#differenz");
+const t_retour = _s("#retour");
+
 
 /* Event Listener */
 
@@ -43,26 +63,50 @@ const _update_warenimport = () => {
     let value_igesamt = _c(i_gesamt.value);
     let value_inach = _c(i_nach.value);
     let value_tzollwert = value_igesamt + value_inach;
-    t_zollwert.textContent = value_tzollwert;
+    t_zollwert.textContent = _ro(value_tzollwert);
 
     let value_izoll = _c(i_zoll.value);
     let value_ieust = _c(i_eust.value);
+    
     let value_tzoll = (value_izoll/100)*value_tzollwert;
-    t_zoll.textContent = value_tzoll;
+    t_zoll.textContent = _ro(value_tzoll);
 
     let value_teust = (value_tzoll + value_tzollwert)*(value_ieust/100);
-    t_eust.textContent = value_teust;
+    t_eust.textContent = _ro(value_teust);
 
     let value_iqi = _c(i_qi.value);
     let value_tgesamtr = value_iqi + value_tzoll + value_tzollwert;
 
-    t_gesamt_r.textContent = value_tgesamtr;
+    t_gesamt_r.textContent = _ro(value_tgesamtr);
 
     let value_iproduk = _c(i_produk.value);
 
     let value_teinheitr = value_tgesamtr/value_iproduk;
-    t_einheit_r.textContent = value_teinheitr;
+    t_einheit_r.textContent = _ro(value_teinheitr);
 
-    let value_eusteinheitr = value_teust/value_iproduk;
-    t_eust_einheit_r.textContent = value_eusteinheitr;
+    let value_teusteinheitr = value_teust/value_iproduk;
+    t_eust_einheit_r.textContent = _ro(value_teusteinheitr);
+
+    let value_iverk = _c(i_verk.value);
+
+    let value_tenthaltene = value_iverk*value_ieust/(100+value_ieust);
+    t_enthaltene.textContent = _ro(value_tenthaltene);
+    
+    let value_iverkauf = _c(i_verkauf.value);
+
+    let value_gebuhren = value_iverkauf/100*value_iverk;
+    t_gebuhren.textContent = _ro(value_gebuhren);
+
+    let value_iversand = _c(i_versand.value);
+    t_kosten.textContent = _ro(value_iversand);
+
+    t_spreis.textContent = _ro(value_teinheitr);
+
+    let value_differenz = _ro(value_tenthaltene - value_teusteinheitr);
+    t_differenz.textContent = value_differenz;
+
+    console.log(_c(i_verkauf.value));
+    let value_retour = (value_iverk - value_gebuhren - _c(i_versand.value) - value_teinheitr - value_differenz)*0.9;
+    t_retour.textContent = _ro(value_retour);
+
 }
--- dist/calculations.dev.js
+++ dist/calculations.dev.js
@@ -1,8 +1,17 @@
 "use strict";
 
+/* Helpers */
+var _c = function _c(t) {
+  return Number(t ? t : 0);
+};
+
 var _s = function _s(n) {
   return document.querySelector(n);
 };
+
+function _ro(n) {
+  return +(Math.round(n + "e+2") + "e-2");
+}
 /* Selecting Inputs */
 
 
@@ -31,12 +40,28 @@ var t_gesamt_r = _s("#gesamt-r");
 var t_einheit_r = _s("#einheit-r");
 
 var t_eust_einheit_r = _s("#eust-einheit-r");
-/* Helpers */
+/* Selecting Inputs - for amazon */
 
 
-var _c = function _c(t) {
-  return Number(t ? t : 0);
-};
+var i_verk = _s("#verk");
+
+var i_verkauf = _s("#verkauf");
+
+var i_versand = _s("#versand");
+/* Selecting Text - for amazon */
+
+
+var t_enthaltene = _s("#enthaltene");
+
+var t_gebuhren = _s("#gebuhren");
+
+var t_kosten = _s("#kosten");
+
+var t_spreis = _s("#spreis");
+
+var t_differenz = _s("#differenz");
+
+var t_retour = _s("#retour");
 /* Event Listener */
 
 
@@ -50,26 +75,48 @@ var _update_warenimport = function _update_warenimport() {
   var value_inach = _c(i_nach.value);
 
   var value_tzollwert = value_igesamt + value_inach;
-  t_zollwert.textContent = value_tzollwert;
+  t_zollwert.textContent = _ro(value_tzollwert);
 
   var value_izoll = _c(i_zoll.value);
 
   var value_ieust = _c(i_eust.value);
 
   var value_tzoll = value_izoll / 100 * value_tzollwert;
-  t_zoll.textContent = value_tzoll;
+  t_zoll.textContent = _ro(value_tzoll);
   var value_teust = (value_tzoll + value_tzollwert) * (value_ieust / 100);
-  t_eust.textContent = value_teust;
+  t_eust.textContent = _ro(value_teust);
 
   var value_iqi = _c(i_qi.value);
 
   var value_tgesamtr = value_iqi + value_tzoll + value_tzollwert;
-  t_gesamt_r.textContent = value_tgesamtr;
+  t_gesamt_r.textContent = _ro(value_tgesamtr);
 
   var value_iproduk = _c(i_produk.value);
 
   var value_teinheitr = value_tgesamtr / value_iproduk;
-  t_einheit_r.textContent = value_teinheitr;
-  var value_eusteinheitr = value_teust / value_iproduk;
-  t_eust_einheit_r.textContent = value_eusteinheitr;
+  t_einheit_r.textContent = _ro(value_teinheitr);
+  var value_teusteinheitr = value_teust / value_iproduk;
+  t_eust_einheit_r.textContent = _ro(value_teusteinheitr);
+
+  var value_iverk = _c(i_verk.value);
+
+  var value_tenthaltene = value_iverk * value_ieust / (100 + value_ieust);
+  t_enthaltene.textContent = _ro(value_tenthaltene);
+
+  var value_iverkauf = _c(i_verkauf.value);
+
+  var value_gebuhren = value_iverkauf / 100 * value_iverk;
+  t_gebuhren.textContent = _ro(value_gebuhren);
+
+  var value_iversand = _c(i_versand.value);
+
+  t_kosten.textContent = _ro(value_iversand);
+  t_spreis.textContent = _ro(value_teinheitr);
+
+  var value_differenz = _ro(value_tenthaltene - value_teusteinheitr);
+
+  t_differenz.textContent = value_differenz;
+  console.log(_c(i_verkauf.value));
+  var value_retour = (value_iverk - value_gebuhren - _c(i_versand.value) - value_teinheitr - value_differenz) * 0.9;
+  t_retour.textContent = _ro(value_retour);
 };
