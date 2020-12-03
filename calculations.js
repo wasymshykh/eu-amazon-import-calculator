01b35f63ea59449bf7bfe4ebed6d9be7d1e7f34c
Wasym <wasymshykh@gmail.com>
2020-12-03 17:15:01 +0500

    updated data and new fields

--- calculations.js
+++ calculations.js
@@ -33,7 +33,6 @@ const t_eust = _s("#eust-r");
 
 const t_gesamt_r = _s("#gesamt-r");
 const t_einheit_r = _s("#einheit-r");
-const t_eust_einheit_r = _s("#eust-einheit-r");
 
 /* Selecting Inputs - for amazon */
 
@@ -44,14 +43,42 @@ const i_versand = _s("#versand");
 /* Selecting Text - for amazon */
 
 const t_enthaltene = _s("#enthaltene");
+const t_netto = _s("#netto");
 const t_gebuhren = _s("#gebuhren");
 const t_kosten = _s("#kosten");
 const t_spreis = _s("#spreis");
-const t_differenz = _s("#differenz");
 const t_retour = _s("#retour");
 
+/* Selecting Inputs - for gewinn */
+
+const i_amtag = _s("#amtag");
+const i_var_kosten_1 = _s("#var-kosten-i-1");
+const i_var_kosten_2 = _s("#var-kosten-i-2");
+const i_var_kosten_3 = _s("#var-kosten-i-3");
+const i_var_kosten_4 = _s("#var-kosten-i-4");
+
+/* Selecting Text - for gewinn */
+
+const t_immonat = _s("#immonat");
+const t_promonat = _s("#promonat");
+const t_promonat2 = _s("#promonat2");
+const t_nettogewinn = _s("#nettogewinn");
 
 /* Event Listener */
+
+const calulate_percentage = (b25, b13) => {
+
+    if (((b25/b13) * 100) > 90) {
+        // b25 is higher than 90%
+        t_retour.parentElement.classList.add('make-green');
+        t_retour.parentElement.classList.remove('make-red');
+    } else {
+        // b25 is lower than 90%
+        t_retour.parentElement.classList.add('make-red');
+        t_retour.parentElement.classList.remove('make-green');
+    }
+    console.log();
+}
 
 const _get_updated = (e) => {
     
@@ -84,13 +111,13 @@ const _update_warenimport = () => {
     let value_teinheitr = value_tgesamtr/value_iproduk;
     t_einheit_r.textContent = _ro(value_teinheitr);
 
-    let value_teusteinheitr = value_teust/value_iproduk;
-    t_eust_einheit_r.textContent = _ro(value_teusteinheitr);
-
     let value_iverk = _c(i_verk.value);
 
     let value_tenthaltene = value_iverk*value_ieust/(100+value_ieust);
     t_enthaltene.textContent = _ro(value_tenthaltene);
+
+    let value_tnetto = value_iverk - value_tenthaltene;
+    t_netto.textContent = _ro(value_tnetto);
     
     let value_iverkauf = _c(i_verkauf.value);
 
@@ -102,11 +129,32 @@ const _update_warenimport = () => {
 
     t_spreis.textContent = _ro(value_teinheitr);
 
-    let value_differenz = _ro(value_tenthaltene - value_teusteinheitr);
-    t_differenz.textContent = value_differenz;
-
-    console.log(_c(i_verkauf.value));
-    let value_retour = (value_iverk - value_gebuhren - _c(i_versand.value) - value_teinheitr - value_differenz)*0.9;
+    let value_retour = (value_tnetto - value_gebuhren - _c(i_versand.value) - value_teinheitr)*0.9;
     t_retour.textContent = _ro(value_retour);
 
+    
+    let value_iamtag = _c(i_amtag.value);
+
+    let value_immonat = value_iamtag *30;
+    t_immonat.textContent = _ro(value_immonat);
+
+    let value_promonat = value_iamtag*value_retour*30;
+    t_promonat.textContent = _ro(value_promonat);
+    t_promonat2.textContent = _ro(value_promonat);
+
+    
+    let value_ivarkosten1 = _c(i_var_kosten_1.value);
+    let value_ivarkosten2 = _c(i_var_kosten_2.value);
+    let value_ivarkosten3 = _c(i_var_kosten_3.value);
+    let value_ivarkosten4 = _c(i_var_kosten_4.value);
+    
+    let value_nettogewinn = value_promonat - value_ivarkosten1 - value_ivarkosten2 - value_ivarkosten3 - value_ivarkosten4;
+    t_nettogewinn.textContent = _ro(value_nettogewinn);
+
+    calulate_percentage(value_retour, value_teinheitr);
+
 }
+
+_update_warenimport();
+
+
--- dist/calculations.dev.js
+++ dist/calculations.dev.js
@@ -38,8 +38,6 @@ var t_eust = _s("#eust-r");
 var t_gesamt_r = _s("#gesamt-r");
 
 var t_einheit_r = _s("#einheit-r");
-
-var t_eust_einheit_r = _s("#eust-einheit-r");
 /* Selecting Inputs - for amazon */
 
 
@@ -53,17 +51,53 @@ var i_versand = _s("#versand");
 
 var t_enthaltene = _s("#enthaltene");
 
+var t_netto = _s("#netto");
+
 var t_gebuhren = _s("#gebuhren");
 
 var t_kosten = _s("#kosten");
 
 var t_spreis = _s("#spreis");
 
-var t_differenz = _s("#differenz");
-
 var t_retour = _s("#retour");
+/* Selecting Inputs - for gewinn */
+
+
+var i_amtag = _s("#amtag");
+
+var i_var_kosten_1 = _s("#var-kosten-i-1");
+
+var i_var_kosten_2 = _s("#var-kosten-i-2");
+
+var i_var_kosten_3 = _s("#var-kosten-i-3");
+
+var i_var_kosten_4 = _s("#var-kosten-i-4");
+/* Selecting Text - for gewinn */
+
+
+var t_immonat = _s("#immonat");
+
+var t_promonat = _s("#promonat");
+
+var t_promonat2 = _s("#promonat2");
+
+var t_nettogewinn = _s("#nettogewinn");
 /* Event Listener */
 
+
+var calulate_percentage = function calulate_percentage(b25, b13) {
+  if (b25 / b13 * 100 > 90) {
+    // b25 is higher than 90%
+    t_retour.parentElement.classList.add('make-green');
+    t_retour.parentElement.classList.remove('make-red');
+  } else {
+    // b25 is lower than 90%
+    t_retour.parentElement.classList.add('make-red');
+    t_retour.parentElement.classList.remove('make-green');
+  }
+
+  console.log();
+};
 
 var _get_updated = function _get_updated(e) {
   _update_warenimport();
@@ -95,13 +129,13 @@ var _update_warenimport = function _update_warenimport() {
 
   var value_teinheitr = value_tgesamtr / value_iproduk;
   t_einheit_r.textContent = _ro(value_teinheitr);
-  var value_teusteinheitr = value_teust / value_iproduk;
-  t_eust_einheit_r.textContent = _ro(value_teusteinheitr);
 
   var value_iverk = _c(i_verk.value);
 
   var value_tenthaltene = value_iverk * value_ieust / (100 + value_ieust);
   t_enthaltene.textContent = _ro(value_tenthaltene);
+  var value_tnetto = value_iverk - value_tenthaltene;
+  t_netto.textContent = _ro(value_tnetto);
 
   var value_iverkauf = _c(i_verkauf.value);
 
@@ -112,11 +146,28 @@ var _update_warenimport = function _update_warenimport() {
 
   t_kosten.textContent = _ro(value_iversand);
   t_spreis.textContent = _ro(value_teinheitr);
-
-  var value_differenz = _ro(value_tenthaltene - value_teusteinheitr);
-
-  t_differenz.textContent = value_differenz;
-  console.log(_c(i_verkauf.value));
-  var value_retour = (value_iverk - value_gebuhren - _c(i_versand.value) - value_teinheitr - value_differenz) * 0.9;
+  var value_retour = (value_tnetto - value_gebuhren - _c(i_versand.value) - value_teinheitr) * 0.9;
   t_retour.textContent = _ro(value_retour);
+
+  var value_iamtag = _c(i_amtag.value);
+
+  var value_immonat = value_iamtag * 30;
+  t_immonat.textContent = _ro(value_immonat);
+  var value_promonat = value_iamtag * value_retour * 30;
+  t_promonat.textContent = _ro(value_promonat);
+  t_promonat2.textContent = _ro(value_promonat);
+
+  var value_ivarkosten1 = _c(i_var_kosten_1.value);
+
+  var value_ivarkosten2 = _c(i_var_kosten_2.value);
+
+  var value_ivarkosten3 = _c(i_var_kosten_3.value);
+
+  var value_ivarkosten4 = _c(i_var_kosten_4.value);
+
+  var value_nettogewinn = value_promonat - value_ivarkosten1 - value_ivarkosten2 - value_ivarkosten3 - value_ivarkosten4;
+  t_nettogewinn.textContent = _ro(value_nettogewinn);
+  calulate_percentage(value_retour, value_teinheitr);
 };
+
+_update_warenimport();
